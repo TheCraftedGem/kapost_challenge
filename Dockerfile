e0a2789a70cd7ac91d363792aef94f03ab63b56c
Emilio <thecraftedgem@tuta.io>
2019-04-02 16:39:13 -0600

    Includes Basic Script Scaffold For Buckets

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-FROM python:2-slim
+FROM python:2.7
 
-RUN mkdir /kapost.py
+RUN mkdir /kapost
 
-WORKDIR /kapost.py
+WORKDIR /kapost
 
-COPY requirements.txt /kapost.py
+COPY requirements.txt /kapost
 RUN pip install –no-cache-dir -r requirements.txt
 
 COPY kapost.py /kapost
 
 ENTRYPOINT [ “python”, “-u”, “./kapost.py”]
+
---
+++ kapost.py
@@ -0,0 +1,20 @@
+# import boto3
+# s3 = boto3.resource('s3')
+# copy_source = {
+#     'Bucket': 'mybucket',
+#     'Key': 'mykey'
+# }
+# s3.meta.client.copy(copy_source, 'otherbucket', 'otherkey')
+
+
+# import boto
+
+# c = boto.connect_s3()
+# src = c.get_bucket('my_source_bucket')
+# dst = c.get_bucket('my_destination_bucket')
+
+# for k in src.list():
+#     # copy stuff to your destination here
+#     dst.copy_key(k.key.name, src.name, k.key.name)
+#     # then delete the source key
+#     k.delete()
