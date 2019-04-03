50b8f0e1bece61db8119ce4e1c2cea3ea084048a
Emilio <thecraftedgem@tuta.io>
2019-04-03 12:12:15 -0600

    Refactored Solution With CLI For Args

--- kapost.py
+++ kapost.py
@@ -1,47 +1,39 @@
-# import boto3
-# import os
-# s3 = boto3.resource('s3')
-# first_bucket = s3.Bucket('177621984')
-# second_bucket = s3.Bucket('198421776')
-
-
+import argparse
 import boto3
-s3 = boto3.resource('s3')
-bucket_1 = s3.Bucket('177621984')
-bucket_2 = s3.Bucket('198421776')
-threshold = 1000MB
 
 
-def filter_by_threshold(bucket_1, bucket_2, threshold):
-    for o in bucket_1.objects.all():
-        if (Content-Length > threshold) in o.key:
-            copy_source = {
-            'Bucket': bucket_1,
+def main(argv):
+    args = cli(argv)
+    src = args.src
+    dst = args.dst
+    threshold = args.size
+
+    s3 = boto3.resource('s3')
+
+    src_bucket = s3.Bucket(src)
+    dst_bucket = s3.Bucket(dst)
+
+    for o in src_bucket.objects.all():
+        if (o.size / (2 ** 20)) > threshold:
+            source = {
+            'Bucket': src_bucket,
             'Key': o.key
             }
-        s3.meta.client.copy(copy_source, 'bucket_2', 'otherkey')
+            dst_bucket.copy(source, o.key)
+    
 
-
-filter_by_threshold(bucket_1, bucket_2, threshold)
-
-
-#Create Conditional That Filters Out Files Less Than Threshhold
-# if (bucket.size > threshold)
-# copy_source = {
-#     'Bucket': first_bucket,
-#     'Key': 'mykey'
-# }
-# s3.meta.client.copy(copy_source, 'otherbucket', 'otherkey')
-
-
-# import boto
-
-# c = boto.connect_s3()
-# src = c.get_bucket('my_source_bucket')
-# dst = c.get_bucket('my_destination_bucket')
-
-# for k in src.list():
-#     # copy stuff to your destination here
-#     dst.copy_key(k.key.name, src.name, k.key.name)
-#     # then delete the source key
-#     k.delete()
+def cli(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", "--from", dest="src", type=str, required=True
+    )
+    parser.add_argument(
+        "-t", "--to", dest="dst", type=str, required=True
+    )
+    parser.add_argument(
+        "-s",
+        "--size",
+        type=int,
+        required=True,
+    )
+    return parser.parse_args(argv)
